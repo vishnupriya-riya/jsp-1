d3659f0f34e42c1ca3aa83737122dec2b6642c98
vishnupriya-riya <vishnupriyavelu18@gmail.com>
2024-08-06 23:59:21 +0530

    Updated

--- 04_JS_Functions/Ch4_pgm_01.js
+++ 04_JS_Functions/Ch4_pgm_01.js
@@ -14,8 +14,27 @@ console.log("Actors: " + movie1.actors);
 console.log("Directors: " + movie1.directors);
 console.log("------------------------------");
 
+var movie2;
 
+movie2 = {
+  title: "The Strangers: Chapter 1",
+  actors: "Renny Harlin",
+  directors: "Jose David Montero, Lionsgate"
+};
 
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+mov1 = {
+  blogpost : "Eleven-year-old Riley has moved to San Francisco, leaving behind her life in Minnesota. "
+}
+console.log(mov1)
+mov2 = {
+  blogpost : "After their car breaks down in an eerie small town, a young couple is forced to spend the night in a remote cabin"
+}
+console.log(mov2)
 /* Further Adventures
  *
  * 1) Add a second movie and display the same info for it.
